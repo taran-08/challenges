b22fe14bf05ef51be848677e3259a655847f0bcc
taran-08 <107032209+taran-08@users.noreply.github.com>
2022-06-16 18:52:26 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,90 +1,82 @@
-const mins=document.querySelector(".mins");
-const secs=document.querySelector(".secs");
-const start=document.querySelector(".start");
-const setting=document.querySelector(".settings");
+const mins = document.querySelector(".mins");
+const secs = document.querySelector(".secs");
+const start = document.querySelector(".start");
+const setting = document.querySelector(".settings");
 var interval;
 
-function settingOf()
-{
+function settingOf() {
     stopTimer();
     allowInput();
 }
-function allowInput()
-{
-    mins.disabled=false;
-    secs.disabled=false;
+function allowInput() {
+    mins.disabled = false;
+    secs.disabled = false;
 }
-function disableInput()
-{
-    mins.disabled=true;
-    secs.disabled=true;
+function disableInput() {
+    mins.disabled = true;
+    secs.disabled = true;
 }
 
-function startStop()
-{
+function startStop() {
     disableInput();
-    if(start.innerHTML === "START")
-    {
-        if(!(mins.value>=00 && mins.value<=60) || !(secs.value>=00 && secs.value<=60) || mins.value=="" || secs.value=="")
-    {
-        alert("Enter the valid time");
-        ring.style.stroke="green";
-        mins.value="00";
-        secs.value="05";
+    if (start.innerHTML === "START") {
+        if ((mins.value > 60 && secs.value > 60) || (mins.value < 0 && secs.value < 0)) {
+            alert("Enter the valid time");
+            ring.style.stroke = "green";
+            mins.value = "00";
+            secs.value = "05";
+        }
+        else if ((mins.value == 00 && secs.value == 0) || (mins.value == 0 && secs.value == 0)) {
+            alert("Enter the valid time");
+            ring.style.stroke = "green";
+            mins.value = "00";
+            secs.value = "05";
+        }
+        else {
+            startTimer();
+        }
     }
-    else
-    {
-        startTimer();
+    else {
+        stopTimer();
     }
-   }
-   else
-   {
-     stopTimer();
-   }
 }
 
-function startTimer()
-{
- 
-    ring.style.stroke="blue";
-    start.innerHTML="PAUSE";
-    interval=setInterval(function() {
-        if(mins.value==0&&secs.value==0)
-        {
-        clearInterval(interval);
-        ring.style.stroke="red";
-        start.innerHTML="START";
-        mins.value="00";
-        secs.value="00";
-        setTimeout(() => {
-            alert("Times up!");
-            ring.style.stroke="green";
-            mins.value="00";
-            secs.value="05";
-        }, 100)
+function startTimer() {
+
+    ring.style.stroke = "blue";
+    start.innerHTML = "PAUSE";
+    interval = setInterval(function () {
+        if (mins.value == 0 && secs.value == 0) {
+            clearInterval(interval);
+            ring.style.stroke = "red";
+            start.innerHTML = "START";
+            mins.value = "00";
+            secs.value = "00";
+            setTimeout(() => {
+                alert("Times up!");
+                ring.style.stroke = "green";
+                mins.value = "00";
+                secs.value = "05";
+            }, 100)
         }
-        else
-        {
-        if(secs.value==0)
-        {
-            mins.value=mins.value-1;
-            mins.value=('0'+mins.value).slice(-2);
-            secs.value="59";
-            
+        else {
+            if (secs.value == 0) {
+                mins.value = mins.value - 1;
+                mins.value = ('0' + mins.value).slice(-2);
+                secs.value = "59";
+
+            }
+            else {
+                secs.value = secs.value - 1;
+                secs.value = ('0' + secs.value).slice(-2);
+            }
         }
-        else
-        {
-            secs.value=secs.value-1;
-            secs.value=('0'+secs.value).slice(-2);
-        }
-       }
     }, 1000);
 
-   }
-function stopTimer()
-{
-    start.innerHTML="START";
-    ring.style.stroke="yellow";
+}
+function stopTimer() {
+    start.innerHTML = "START";
+    ring.style.stroke = "yellow";
     clearInterval(interval);
 
 }
