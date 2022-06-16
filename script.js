2a11b45e9c3eb3644fdfc784dc5509c5c61df26c
taran-08 <107032209+taran-08@users.noreply.github.com>
2022-06-16 15:30:14 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,90 @@
+const mins=document.querySelector(".mins");
+const secs=document.querySelector(".secs");
+const start=document.querySelector(".start");
+const setting=document.querySelector(".settings");
+var interval;
+
+function settingOf()
+{
+    stopTimer();
+    allowInput();
+}
+function allowInput()
+{
+    mins.disabled=false;
+    secs.disabled=false;
+}
+function disableInput()
+{
+    mins.disabled=true;
+    secs.disabled=true;
+}
+
+function startStop()
+{
+    disableInput();
+    if(start.innerHTML === "START")
+    {
+        if(!(mins.value>=0 && mins.value<=60) || !(secs.value>=0 && secs.value<=60) || mins.value=="" || secs.value=="")
+    {
+        console.log("perfect0");
+        alert("Enter the valid time");
+        ring.style.stroke="green";
+        mins.value="00";
+        secs.value="00";
+    }
+    else
+    {
+       
+        startTimer();
+    }
+   }
+   else
+   {
+     stopTimer();
+   }
+}
+
+function startTimer()
+{
+ 
+    ring.style.stroke="blue";
+    start.innerHTML="PAUSE";
+    interval=setInterval(function() {
+        if(mins.value==0&&secs.value==0)
+        {
+        clearInterval(interval);
+        ring.style.stroke="red";
+        start.innerHTML="START";
+        mins.value="00";
+        secs.value="00";
+        setTimeout(() => {
+            alert("Times up!");
+            ring.style.stroke="green";
+        }, 100)
+        }
+        else
+        {
+        if(secs.value==0)
+        {
+            mins.value=mins.value-1;
+            mins.value=('0'+mins.value).slice(-2);
+            secs.value="59";
+            
+        }
+        else
+        {
+            secs.value=secs.value-1;
+            secs.value=('0'+secs.value).slice(-2);
+        }
+       }
+    }, 1000);
+
+   }
+function stopTimer()
+{
+    start.innerHTML="START";
+    ring.style.stroke="yellow";
+    clearInterval(interval);
+
+}
