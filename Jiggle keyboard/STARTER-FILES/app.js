0eb2084c8f6ae79c3a0f8e3280b46eb07456fedd
Taranjot Singh <taranjotsingh.loomba@cawstudios.com>
2022-06-17 16:37:48 +0530

    saved

--- Jiggle keyboard/STARTER-FILES/app.js
+++ Jiggle keyboard/STARTER-FILES/app.js
@@ -3,6 +3,7 @@ console.log(elem);
 const random= Math.floor(Math.random()*elem.length-1);
 console.log(random);
 elem[random].classList.add("jiggle");
-function recheck({
+recheck=()=>{
+
     
-});
+};
