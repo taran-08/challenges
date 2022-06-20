a3cf5d1410c2494da7b744a058b7c0070b378071
Taranjot Singh <taranjotsingh.loomba@cawstudios.com>
2022-06-20 14:01:00 +0530

    adding new challenge

---
+++ STARTER-FILES/app.js
@@ -0,0 +1,10 @@
+const keys = document.getElementsByTagName('a');
+const playing = (i) => {
+  const audio = new Audio(`audio/key- ${i + 1} .mp3`);
+  audio.play();
+};
+for (let i = 0; i < keys.length; i += 1) {
+  keys[i].addEventListener('click', () => {
+    playing(i);
+  }); 
+}
