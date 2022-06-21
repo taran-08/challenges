11355fc9a1ba5505f0165cc6078dfe7d25a4fe61
Taranjot Singh <taranjotsingh.loomba@cawstudios.com>
2022-06-21 17:36:49 +0530

    commit

--- pomodoro/script.js
+++ pomodoro/script.js
@@ -1,31 +1,34 @@
-import { allowInput, disableInput, isValidTime, startTimer, stopTimer } from "./util.js";
-export let mins = document.querySelector(".mins");
-export let secs = document.querySelector(".secs");
-export let start = document.querySelector(".start");
-export let setting = document.querySelector(".settings");
-let interval;
+import {
+  allowInput,
+  disableInput,
+  isValidTime,
+  startTimer,
+  stopTimer,
+} from './util.js';
+export const mins = document.querySelector('.mins');
+export const secs = document.querySelector('.secs');
+export const start = document.querySelector('.start');
+export const setting = document.querySelector('.settings');
 
 const settingOf = () => {
-    stopTimer();
-    allowInput();
-}
+  stopTimer();
+  allowInput();
+};
 const startStop = () => {
-    disableInput();
-    if (start.innerHTML === "START") {
-        if (isValidTime()) {
-            alert("Enter the valid time");
-            ring.style.stroke = "green";
-            mins.value = "00";
-            secs.value = "05";
-        }
-        else {
-            startTimer();
-        }
+  disableInput();
+  if (start.innerHTML === 'START') {
+    if (isValidTime()) {
+      alert('Enter the valid time');
+      document.ring.style.stroke = 'green';
+      mins.value = '00';
+      secs.value = '05';
+    } else {
+      startTimer();
     }
-    else {
-        stopTimer();
-    }
-}
+  } else {
+    stopTimer();
+  }
+};
 
-start.addEventListener("click", startStop);
-setting.addEventListener("click", settingOf);
+start.addEventListener('click', startStop);
+setting.addEventListener('click', settingOf);
