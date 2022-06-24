e47cc72917a7adcd6acca729ab14c655060a8510
Taranjot Singh <taranjotsingh.loomba@cawstudios.com>
2022-06-24 13:48:16 +0530

    commit

--- Challenge#1Pomodoro/util.js
+++ Challenge#1Pomodoro/util.js
@@ -1,58 +1,58 @@
 import {mins, secs, start} from './script.js';
 let interval;
 export const allowInput = () => {
-  mins.disabled = false;
-  secs.disabled = false;
+    mins.disabled = false;
+    secs.disabled = false;
 };
 export const disableInput = () => {
-  mins.disabled = true;
-  secs.disabled = true;
+    mins.disabled = true;
+    secs.disabled = true;
 };
 export const isValidTime = () => {
-  if (
-    mins.value.toString().indexOf('.') != -1 ||
-    secs.value.toString().indexOf('.') != -1
-  ) {
-    return true;
-  } else if (
-    (mins.value > 0 && mins.value < 60) ||
+    if (
+        mins.value.toString().indexOf('.') != -1 ||
+        secs.value.toString().indexOf('.') != -1
+    ) {
+        return true;
+    } else if (
+        (mins.value > 0 && mins.value < 60) ||
         (secs.value > 0 && secs.value < 60)
-  ) {
-    return false;
-  } else {
-    return true;
-  }
+    ) {
+        return false;
+    } else {
+        return true;
+    }
 };
 export const startTimer = () => {
-  ring.style.stroke = 'blue';
-  start.innerHTML = 'PAUSE';
-  interval = setInterval(function() {
-    if (mins.value == 0 && secs.value == 0) {
-      clearInterval(interval);
-      ring.style.stroke = 'red';
-      start.innerHTML = 'START';
-      mins.value = '00';
-      secs.value = '00';
-      setTimeout(() => {
-        alert('Times up!');
-        ring.style.stroke = 'green';
-        mins.value = '00';
-        secs.value = '05';
-      }, 100);
-    } else {
-      if (secs.value == 0) {
-        mins.value = mins.value - 1;
-        mins.value = ('0' + mins.value).slice(-2);
-        secs.value = '59';
-      } else {
-        secs.value = secs.value - 1;
-        secs.value = ('0' + secs.value).slice(-2);
-      }
-    }
-  }, 1000);
+    ring.style.stroke = 'blue';
+    start.innerHTML = 'PAUSE';
+    interval = setInterval(function() {
+        if (mins.value == 0 && secs.value == 0) {
+            clearInterval(interval);
+            ring.style.stroke = 'red';
+            start.innerHTML = 'START';
+            mins.value = '00';
+            secs.value = '00';
+            setTimeout(() => {
+                alert('Times up!');
+                ring.style.stroke = 'green';
+                mins.value = '00';
+                secs.value = '05';
+            }, 100);
+        } else {
+            if (secs.value == 0) {
+                mins.value = mins.value - 1;
+                mins.value = ('0' + mins.value).slice(-2);
+                secs.value = '59';
+            } else {
+                secs.value = secs.value - 1;
+                secs.value = ('0' + secs.value).slice(-2);
+            }
+        }
+    }, 1000);
 };
 export const stopTimer = () => {
-  start.innerHTML = 'START';
-  ring.style.stroke = 'yellow';
-  clearInterval(interval);
+    start.innerHTML = 'START';
+    ring.style.stroke = 'yellow';
+    clearInterval(interval);
 };
